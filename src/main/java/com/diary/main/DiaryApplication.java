c9d705831beeb05f96e6a92b8f2a99891560805e
hao <1936705930@qq.com>
2020-04-12 12:37:52 +0800

    修改了一些bug.

--- src/main/java/com/diary/main/DiaryApplication.java
+++ src/main/java/com/diary/main/DiaryApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @MapperScan("com.diary.main.mapper")
 @EnableCaching
+@EnableAsync
 public class DiaryApplication {
     public static void main(String[] args) {
         System.setProperty("es.set.netty.runtime.available.processors", "false");
